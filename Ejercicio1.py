2d0e3f4476712a2fe7aadeab5db7a48bbb71ee11
saulgn2007 <saulgonzaleznavarro2007@gmail.com>
2025-02-13 09:39:23 +0100

    Ejercicio1

---
+++ Ejercicio1.py
@@ -0,0 +1,11 @@
+class persona:
+    def __init__(self, nombre, edad, profesion):
+        self.nombre = nombre
+        self.edad = edad
+        self.profesion = profesion
+    def presentarse(self):
+        print(f"Hola, mi nombre es {self.nombre}, tengo {self.edad} años y soy {self.profesion}.")
+
+p = persona("Ana", 32 , "arquitecta")
+
+p.presentarse()
