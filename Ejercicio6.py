fa58dc12d0749ed51b3bebc78f6e27d9bb2eae11
saulgn2007 <saulgonzaleznavarro2007@gmail.com>
2025-02-20 08:55:45 +0100

    Ejercicio 6

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -1,3 +1,26 @@
 # Crea una clase Libro con atributos titulo y autor. Luego, crea una clase Biblioteca 
 # que contenga una lista de libros y métodos para agregar libros y mostrarlos.
 
+class Libro:
+    def __init__(self, titulo, autor):
+        self.titulo = titulo
+        self.autor = autor
+
+class Biblioteca:
+    def __init__(self):
+        self.libros = []
+
+    def agregar_libro(self, libro):
+        self.libros.append(libro)
+
+    def mostrar_libros(self):
+        for libro in self.libros:
+            print(f"Libro: {libro.titulo}, Autor: {libro.autor}")
+
+libro1 = Libro("El principito", "Antoine de Saint-Exupéry")
+libro2 = Libro("El señor de los anillos", "J.R.R. Tolkien")
+biblioteca = Biblioteca()
+biblioteca.agregar_libro(libro1)
+biblioteca.agregar_libro(libro2)
+biblioteca
+biblioteca.mostrar_libros()
